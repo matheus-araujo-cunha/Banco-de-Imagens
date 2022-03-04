3bc9cadfc163b98a26107720061e7c384849c593
Matheus Araujo Cunha <matheusc2005@hotmail.com>
2022-03-04 12:14:33 -0300

    fix: refactoring routes upload and retrieve_files

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from http import HTTPStatus
 import os
 from werkzeug.utils import secure_filename
 
-#from app.kenzie.image import FILES_DIRECTORY
 from app.kenzie import list_by_extension,checking_file_size, get_file_path,validate_extension_file, validate_name_file, list_all_files
 from app.kenzie.image import ALLOWED_EXTENSIONS, FILES_DIRECTORY
 app = Flask(__name__)
@@ -24,13 +23,13 @@ if not file_exist:
 
 @app.get("/download/<file_name>")
 def download_by_filename(file_name:str):
-    return
+    return ""
 
 @app.get("/download-zip")
-def download_zipped_files():
+def download_dir_as_zip():
     file_extension = request.args.get("file_extension")
     compression_ratio = request.args.get("compression_ratio")
-    return    
+    return ""   
 
 @app.get("/files")
 def retrieve_files():
@@ -47,7 +46,7 @@ def retrieve_files_by_extension(extension:str):
         return pe.args[0],HTTPStatus.NOT_FOUND    
 
 @app.post("/upload")
-def upload_file():
+def upload():
    
     file = request.files["file"]
     content_length = request.headers["Content-Length"]
@@ -57,7 +56,7 @@ def upload_file():
     try:
         checking_file_size(content_length)
         validate_extension_file(extension_file)
-        validate_name_file(file.filename,extension_file)
+        validate_name_file(file.filename)
 
     
         abs_path = os.path.abspath(f"{FILES_DIRECTORY}/{extension_file}")
@@ -72,21 +71,4 @@ def upload_file():
         return fee.args[0],HTTPStatus.CONFLICT
     except PermissionError as pe:
         return pe.args[0],HTTPStatus.UNSUPPORTED_MEDIA_TYPE
-
    
-
-
-    
-    
-    
-
-        
-
- 
-        
- 
-
-    
-    # abs_path = os.path.abspath("fFILES_DIRECTORY")
-    # file_path = safe_join(abs_path,file)
-    return  ""          
--- app/kenzie/image.py
+++ app/kenzie/image.py
@@ -1,5 +1,3 @@
-# Desenvolva sua lógica de manipulação das imagens aqui
-from io import UnsupportedOperation
 import os
 from flask import safe_join
 
@@ -9,23 +7,8 @@ MAX_CONTENT_LENGTH = os.getenv("MAX_CONTENT_LENGTH")
 ALLOWED_EXTENSIONS = os.getenv("ALLOWED_EXTENSIONS")
 
 def checking_file_size(content_length):
-    float_content = ""
-    lenght_content = len(float_content)
     
-    for index,number in enumerate(reversed(content_length),start=1):
-        if index % 3 == 0 and not index == lenght_content:
-            float_content += number+"."
-        else:
-            float_content += number    
-    
-    float_content = float(float_content[lenght_content::-1])
-
-    
-    converted_to_megabyte = float_content / int(MAX_CONTENT_LENGTH)
-
-
-    
-    if converted_to_megabyte > int(MAX_CONTENT_LENGTH):
+    if int(content_length) > int(MAX_CONTENT_LENGTH):
         raise ValueError({
             "error":"File size is invalid",
             "expected":"File size less than 1MB"
@@ -37,13 +20,17 @@ def get_file_path(filename:str):
     filepath = safe_join(abs_path,filename)
     return filepath
 
-def validate_name_file(filename,extension_file):
-    list_files_extension = list(os.listdir(f"{FILES_DIRECTORY}/{extension_file}"))
-    
-    if filename in list_files_extension:
-        raise FileExistsError({
-            "error":"File name already exists"
-        }) 
+def validate_name_file(filename):
+    list_files = walk_files()
+   
+    for _,_,file in list_files:
+        if filename in file:
+            raise FileExistsError({
+                "error":"File name already exists"
+            }) 
+
+def walk_files():
+    return os.walk(FILES_DIRECTORY)
 
 def validate_extension_file(extension_file):
 
@@ -61,12 +48,6 @@ def list_by_extension(extension_file):
     return list_files_extension
 
 def list_all_files():
-    all_files = os.walk(FILES_DIRECTORY)
+    all_files = walk_files()
 
-    response = []
-
-    for _,_,files in all_files:
-        for file in files:
-            response.append(file)    
-
-    return response
+    return [all_files for _,_,file in all_files for all_files in file]
